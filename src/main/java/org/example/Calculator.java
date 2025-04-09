0f9199c8939a364bfb6991bbd7d67e47d2771d9f
NguyenHuboo <nguyenhaxuannguyen@gmail.com>
2025-04-09 14:58:29 +0700

    first commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Calculator {
+
+    public Integer sum(Integer a, Integer b) {
+        return 0;
+    }
+
+    public Integer sub(Integer a, Integer b) {
+        return 0;
+    }
+}
