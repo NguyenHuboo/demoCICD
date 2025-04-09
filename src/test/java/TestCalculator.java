5aabaae1c0d837f8ad78d956f74f176e224e9946
NguyenHuboo <nguyenhaxuannguyen@gmail.com>
2025-04-09 15:14:23 +0700

    test fail

---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,18 @@
+import org.example.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCalculator {
+
+    @Test
+    public void testSum() {
+        Calculator calc = new Calculator();
+        Assert.assertEquals(calc.sum(3, 4), 7);
+    }
+
+    @Test
+    public void testSub() {
+        Calculator calc = new Calculator();
+        Assert.assertEquals(calc.sum(4, 3), 1);
+    }
+}
